2a86f792119dd0d9b3020d40f40b6c198c2a11d2
Samuel Ballan <sgb4622@gmail.com>
2016-07-08 12:14:23 -0400

    model factory

---
+++ proxyDb/factories/model.factory.js
@@ -0,0 +1,24 @@
+module.exports = class ModelFactory {
+  constructor(ModelStrategy) {
+    
+    /**
+     * Makes ProxyModels using the Strategy with the class was constructed
+     * @method modelFactory
+     * @param  {String}     modelName Name of the ModelStrategy
+     * @param  {dbModel}    dbModel   A model from the database.
+     * @return {ProxyModel}           A Proxified version of this model.
+     */
+    return function modelFactory(modelName, dbModel) {
+      class ProxyModel extends ModelStrategy {}
+      
+      ProxyModel.modelName = modelName;
+      ProxyModel.dbModel = dbModel;
+      return ProxyModel;
+    }
+  }
+
+}
+
+
+
+
