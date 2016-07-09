d2b249fbc85efaf63a8a54c136b84d5c537e23f1
Samuel Ballan <sgb4622@gmail.com>
2016-07-08 23:11:39 -0400

    starting to wire up models. should switch to new branch

--- proxyDb/index.js
+++ proxyDb/index.js
@@ -1,22 +1,39 @@
 const Manager = require('./manager');
+const Strategies = require('../strategies')();
 
 class ProxyDb {
-	constructor(strategyName, config) {
+	constructor(strategyName, config={}) {
 		config.ProxyDb = ProxyDb;
 
 		if(!!config.strategyPath) {
 			ProxyDb.strategies[strategyName] = require(config.path);
 		}
 
+		if(Strategies[strategyName]) {
+			ProxyDb.strategies[strategyName] = Strategies[strategyName];
+			config.strategy = ProxyDb.strategies[strategyName];
+		}
+
 		return new Manager(strategyName, config)
 	}
 
-	static addConnection() {
-
+	static addConnection(URI, dbName) {
+		if(dbName) URI = `${URI}/${dbName}`;
+		else dbName = URI;
+		this.connections[dbName] = URI;
 	}
 
-	static addStrategy(name, path='../../strategies/mongoose-strategy') {
-		ProxyDb.strategies[name] = require(path);
+	static addStrategy(name, path=Strategies[name]) {
+		let strategy;
+
+		if(typeof path === 'string') {
+			strategy = Strategies[name] = require(path)
+		} else if(!!path) {
+			strategy = path;
+		} else {
+			strategy = Strategies[name];
+		}
+		ProxyDb.strategies[name] = strategy;
 	}
 }
 
--- proxyDb/manager/manager.factory.js
+++ proxyDb/manager/manager.factory.js
@@ -1,11 +1,29 @@
 const Factories = require('../factories');
 
 class Manager {
-  constructor(strategyName, config) {
+  constructor(strategyName, config={}) {
     this.ProxyDb = config.ProxyDb;
     this.strategy = this.ProxyDb.strategies[strategyName];
-    this.model = Factories.model(this.strategy.model);
+    this.models = {};
   }
+
+  model(...args) {
+    return new Factories.model(this.strategy.model);
+  }
+
+  get connection() {
+    return this.strategy.connection;
+  }
+
+  get instance() {
+    return this.strategy.instance;
+  }
+
+  setConnection(URI) {
+    this.ProxyDb.addConnection(URI)
+  }
+
+
 }
 
 module.exports = Manager;
