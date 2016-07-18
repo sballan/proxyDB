870071241e9ad8e88cee6cbe4a8f082db559b6cf
Samuel Ballan <sgb4622@gmail.com>
2016-07-18 15:13:09 -0400

    need to finish schema

--- demo/mongo-app/app/db.js
+++ demo/mongo-app/app/db.js
@@ -3,6 +3,12 @@ var ProxyDb = require('proxydb');
 var db = new ProxyDb('mongoose');
 
 module.exports = Promise.resolve(db);
+db.connection('mongodb://localhost:27017', 'proxyDb-mongo-app')
+	.open()
+
+var UserSchema = new db.Schema({name: String, age: Number});
+var UserModel = db.model('User', UserSchema);
+
 
 console.log(db);
 
