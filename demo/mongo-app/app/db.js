cea36d96191f317375a0c3edf83f5983169ac9ce
Samuel Ballan <sgb4622@gmail.com>
2016-08-15 15:04:27 -0400

    fixed proxy alpha branch

--- demo/mongo-app/app/db.js
+++ demo/mongo-app/app/db.js
@@ -4,20 +4,17 @@ var chalk = require('chalk')
 var db = new ProxyDb('mongoose');
 
 
-
-
 var connection = db.connection('mongodb://localhost:27017', 'proxyDb-mongo-app')
 
-var startDb = new Promise(function(resolve, reject) {
+var startDb = new Promise(function (resolve, reject) {
 	connection.open(resolve)
 });
 
-startDb.then(function(connection) {
+startDb.then(function (connection) {
 	console.log(chalk.green('db Opened'))
-	var UserSchema = new db.Schema({name: String, age: Number});
-	var UserModel = db.model('User', UserSchema);	
+	var UserSchema = new db.Schema({ name: String, age: Number });
+	var UserModel = db.model('User', UserSchema);
 })
 
 
 module.exports = startDb
-
